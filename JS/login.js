baec25d9985b7e9a9a875fdb985890e795f7a986
The76er <jason.w.cartwright@gmail.com>
2019-03-14 14:13:15 -0700

    Pushing first version of midterm.

---
+++ JS/login.js
@@ -0,0 +1,32 @@
+var Login = /** @class */ (function () {
+    function Login() {
+    }
+    Login.prototype.login = function () {
+        var usernameInput = document.getElementById('username');
+        var passwordInput = document.getElementById('password');
+        var username = usernameInput.value;
+        var password = passwordInput.value;
+        if (username === 'lphan' && password === 'abc123') {
+            this.goToHome();
+        }
+        else {
+            this.displayError();
+        }
+    };
+    Login.prototype.displayError = function () {
+        var errorBox = document.getElementById('error-box');
+        errorBox.innerHTML = 'User not found!';
+        errorBox.innerHTML;
+        ;
+        errorBox.style = "display: block;";
+        var interval = setInterval(function () {
+            errorBox.style = "display: none;";
+            clearInterval(interval);
+        }, 5000);
+    };
+    Login.prototype.goToHome = function () {
+        window.location.href = 'index.html';
+    };
+    return Login;
+}());
+var login = new Login();
